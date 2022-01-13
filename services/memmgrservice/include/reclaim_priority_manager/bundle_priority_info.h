543faba3a218cbdc5d8cd472072b5b9b9059f4a5
ChenJie <chenjie174@huawei.com>
2022-01-13 10:06:31 +0800

    add memmgr fwk
    Signed-off-by: ChenJie <chenjie174@huawei.com>

---
+++ services/memmgrservice/include/reclaim_priority_manager/bundle_priority_info.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2021 XXXX.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_MEMORY_MEMMGR_BUNDLE_PRIORITY_INFO_H
+#define OHOS_MEMORY_MEMMGR_BUNDLE_PRIORITY_INFO_H
+
+#include "reclaim_priority_constants.h"
+#include "process_priority_info.h"
+#include <string>
+#include <list>
+#include <map>
+
+namespace OHOS {
+namespace Memory {
+class BundlePriorityInfo {
+public:
+    using ProcessesInfoMap = std::map<pid_t, ProcessPriorityInfo>;
+    explicit BundlePriorityInfo(std::string name, int bundleUid);
+    explicit BundlePriorityInfo(std::string name, int bundleUid, int priority);
+    ~BundlePriorityInfo();
+    inline bool operator<(const BundlePriorityInfo &tmp) const
+    {
+        return priority_ < tmp.priority_;
+    }
+    std::string name_;
+    int uid_;
+    ProcessesInfoMap processes_;
+    int priority_;
+
+    bool ProcessExistInBundle(pid_t pid);
+    void AddProcess(ProcessPriorityInfo &newProcess);
+    void RemoveProcessById(pid_t pid);
+    ProcessPriorityInfo& FindProcessInfoById(pid_t pid);
+    int GetProcessCount();
+    int GetMinProcessPriority();
+    void SetPriority(int targetPriority);
+    void UpdatePriority();
+};
+} // namespace Memory
+} // namespace OHOS
+#endif // OHOS_MEMORY_MEMMGR_BUNDLE_PRIORITY_INFO_H
---
+++ services/memmgrservice/src/event/app_state_callback_mem_host.cpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2021 XXXX.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "app_state_callback_mem_host.h"
+#include "memmgr_log.h"
+#include "reclaim_priority_manager.h"
+
+namespace OHOS {
+namespace Memory {
+namespace {
+const std::string TAG = "AppStateCallbackMemHost";
+}
+
+AppStateCallbackMemHost::AppStateCallbackMemHost() : appMgrClient_(std::make_unique<AppExecFwk::AppMgrClient>())
+{
+}
+
+AppStateCallbackMemHost::~AppStateCallbackMemHost()
+{
+}
+
+bool AppStateCallbackMemHost::ConnectAppMgrService()
+{
+    if (appMgrClient_ == nullptr) {
+        return false;
+    }
+    int result = static_cast<int>(appMgrClient_->ConnectAppMgrService());
+    return result == ERR_OK;
+}
+
+bool AppStateCallbackMemHost::Register()
+{
+    int result = static_cast<int>(appMgrClient_->RegisterAppStateCallback(sptr<AppStateCallbackMemHost>(this)));
+    return result == ERR_OK;
+}
+
+void AppStateCallbackMemHost::OnAppStateChanged(const AppExecFwk::AppProcessData &appData)
+{
+    std::string appName = appData.appName;
+    HILOGI("called appName=<%{public}s>", appName.c_str());
+    switch (appData.appState) {
+        case AppExecFwk::ApplicationState::APP_STATE_CREATE:
+            ReclaimPriorityManager::GetInstance().UpdateReclaimPriority(appData.pid, appData.uid,
+                appData.appName, AppStateUpdateReason::CREATE_PROCESS);
+            break;
+        case AppExecFwk::ApplicationState::APP_STATE_READY:
+            ReclaimPriorityManager::GetInstance().UpdateReclaimPriority(appData.pid, appData.uid,
+                appData.appName, AppStateUpdateReason::PROCESS_READY);
+            break;
+        case AppExecFwk::ApplicationState::APP_STATE_FOREGROUND:
+            ReclaimPriorityManager::GetInstance().UpdateReclaimPriority(appData.pid, appData.uid,
+                appData.appName, AppStateUpdateReason::FOREGROUND);
+            break;
+        case AppExecFwk::ApplicationState::APP_STATE_BACKGROUND:
+            ReclaimPriorityManager::GetInstance().UpdateReclaimPriority(appData.pid, appData.uid,
+                appData.appName, AppStateUpdateReason::BACKGROUND);
+            break;
+        case AppExecFwk::ApplicationState::APP_STATE_SUSPENDED:
+            ReclaimPriorityManager::GetInstance().UpdateReclaimPriority(appData.pid, appData.uid,
+                appData.appName, AppStateUpdateReason::PROCESS_SUSPEND);
+            break;
+        case AppExecFwk::ApplicationState::APP_STATE_TERMINATED:
+            ReclaimPriorityManager::GetInstance().UpdateReclaimPriority(appData.pid, appData.uid,
+                appData.appName, AppStateUpdateReason::PROCESS_TERMINATED);
+            break;
+        default:
+            break;
+    }
+    // notify kill stratagy manager
+
+    // notify reclaim stratagy manager
+}
+} // namespace Memory
+} // namespace OHOS
