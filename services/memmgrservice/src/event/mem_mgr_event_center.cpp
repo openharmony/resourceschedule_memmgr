0d871a8b9e2a7d203773e62d6b837ab40c41d767
ChenJie <chenjie174@huawei.com>
2022-03-04 10:33:38 +0800

    update
    Signed-off-by: ChenJie <chenjie174@huawei.com>

--- services/memmgrservice/src/event/mem_mgr_event_center.cpp
+++ services/memmgrservice/src/event/mem_mgr_event_center.cpp
@@ -14,11 +14,11 @@
  */
 
 #include "mem_mgr_event_center.h"
+#include <string>
 #include "memmgr_log.h"
 #include "mem_mgr_event_observer.h"
 #include "reclaim_priority_manager.h"
 #include "background_task_mgr_helper.h"
-#include <string>
 
 namespace OHOS {
 namespace Memory {
