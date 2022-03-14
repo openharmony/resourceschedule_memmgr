3b2cca33a43fd111ca5d0c3c800480f69747ac6b
陈杰 <chenjie174@huawei.com>
2022-03-14 15:17:18 +0000

    update common/include/memmgr_ptr_util.h. Signed-off-by: ChenJie <chenjie174@huawei.com>

--- common/include/memmgr_ptr_util.h
+++ common/include/memmgr_ptr_util.h
@@ -24,7 +24,6 @@ namespace Memory {
 
 #define DECLARE_UNIQUE_POINTER(classType, ptrName) std::unique_ptr<classType> ptrName = nullptr
 
-// ptrType: shared / unique
 #define MAKE_POINTER(ptrSelf, ptrType, classType, errLog, errAction, ...)   \
     do {                                                                    \
         ptrSelf = nullptr;                                                  \
