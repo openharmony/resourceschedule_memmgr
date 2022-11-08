bf5f63efc6ecb9423296e16e6ff9ce52c75c401a
wonghiu45 <huangxiao35@huawei.com>
2022-11-08 16:18:38 +0800

    update reclaim_strategy_manager test
    category:bugfix
    issue:#I6034S
    
    Signed-off-by: wonghiu45 <huangxiao35@huawei.com>
    Change-Id: I3c0633d78813cc6e9901a31065617e41900b6741

--- test/unittest/phone/avail_buffer_manager_test.cpp
+++ test/unittest/phone/avail_buffer_manager_test.cpp
@@ -58,5 +58,80 @@ HWTEST_F(AvailBufferManagerTest, initTest, TestSize.Level1)
     MemmgrConfigManager::GetInstance().Init();
     AvailBufferManager::GetInstance().Init();
 }
+
+HWTEST_F(AvailBufferManagerTest, WriteAvailBufferToKernelTest, TestSize.Level1)
+{
+    EXPECT_EQ(AvailBufferManager::GetInstance().WriteAvailBufferToKernel(), true);
+}
+
+HWTEST_F(AvailBufferManagerTest, SetAvailBufferTest, TestSize.Level1)
+{
+    unsigned int availBuffer = 123;
+    unsigned int minAvailBuffer = 100;
+    unsigned int highAvailBuffer = 666;
+    unsigned int swapReserve = 888;
+    std::shared_ptr<AvailBufferConfig> para = std::make_shared<AvailBufferConfig>(availBuffer, minAvailBuffer,
+            highAvailBuffer, swapReserve);
+    EXPECT_EQ(AvailBufferManager::GetInstance().SetAvailBuffer(para), true);
+}
+
+HWTEST_F(AvailBufferManagerTest, LoadAvailBufferFromConfigTest, TestSize.Level1)
+{
+    EXPECT_EQ(AvailBufferManager::GetInstance().LoadAvailBufferFromConfig(), true);
+}
+
+HWTEST_F(AvailBufferManagerTest, CloseZswapdTest, TestSize.Level1)
+{
+    AvailBufferManager::GetInstance().CloseZswapd();
+    EXPECT_EQ(AvailBufferManager::GetInstance().availBuffer_, 0);
+    EXPECT_EQ(AvailBufferManager::GetInstance().minAvailBuffer_, 0);
+    EXPECT_EQ(AvailBufferManager::GetInstance().highAvailBuffer_, 0);
+    EXPECT_EQ(AvailBufferManager::GetInstance().swapReserve_, 0);
+}
+
+HWTEST_F(AvailBufferManagerTest, InitAvailBufferTest, TestSize.Level1)
+{
+    AvailBufferManager::GetInstance().zramEnable_ = false;
+    EXPECT_EQ(AvailBufferManager::GetInstance().zramEnable_, false);
+    AvailBufferManager::GetInstance().InitAvailBuffer();
+    EXPECT_EQ(AvailBufferManager::GetInstance().zramEnable_, true);
+}
+
+HWTEST_F(AvailBufferManagerTest, InitTest, TestSize.Level1)
+{
+    EXPECT_EQ(AvailBufferManager::GetInstance().Init(), true);
+}
+
+HWTEST_F(AvailBufferManagerTest, GetEventHandlerTest, TestSize.Level1)
+{
+    EXPECT_EQ(AvailBufferManager::GetInstance().GetEventHandler(), true);
+}
+
+HWTEST_F(AvailBufferManagerTest, UpdateZramEnableFromKernelTest, TestSize.Level1)
+{
+    AvailBufferManager::GetInstance().zramEnable_ = false;
+    EXPECT_EQ(AvailBufferManager::GetInstance().zramEnable_, false);
+    AvailBufferManager::GetInstance().UpdateZramEnableFromKernel();
+    EXPECT_EQ(AvailBufferManager::GetInstance().zramEnable_, true);
+}
+
+HWTEST_F(AvailBufferManagerTest, CheckAvailBufferTest, TestSize.Level1)
+{
+    unsigned int availBuffer = 123;
+    unsigned int minAvailBuffer = 100;
+    unsigned int highAvailBuffer = 666;
+    unsigned int swapReserve = 888;
+    std::shared_ptr<AvailBufferConfig> para = std::make_shared<AvailBufferConfig>(availBuffer, minAvailBuffer,
+            highAvailBuffer, swapReserve);
+    EXPECT_EQ(AvailBufferManager::GetInstance().CheckAvailBuffer(para), true);
+}
+
+HWTEST_F(AvailBufferManagerTest, UpdateMemTotalFromKernelTest, TestSize.Level1)
+{
+    AvailBufferManager::GetInstance().memTotal_ = 0;
+    EXPECT_EQ(AvailBufferManager::GetInstance().memTotal_, 0);
+    AvailBufferManager::GetInstance().UpdateMemTotalFromKernel();
+    EXPECT_NE(AvailBufferManager::GetInstance().memTotal_, 0);
+}
 }
 }
--- test/unittest/phone/memcg_test.cpp
+++ test/unittest/phone/memcg_test.cpp
@@ -145,5 +145,80 @@ HWTEST_F(MemcgTest, ReadScoreAndReclaimRatiosFromKernelTest, TestSize.Level1)
     delete memcg;
     memcg = nullptr;
 }
+
+HWTEST_F(MemcgTest, SetRatiosByValueTest, TestSize.Level1)
+{
+    unsigned int mem2zramRatio = 12345;
+    unsigned int zram2ufsRatio = 54321;
+    unsigned int refaultThreshold = 66666;
+    ReclaimRatios ratios(mem2zramRatio, zram2ufsRatio, refaultThreshold);
+    ratios.SetRatiosByValue(mem2zramRatio, zram2ufsRatio, refaultThreshold);
+    EXPECT_EQ(ratios.mem2zramRatio_, 100);
+    EXPECT_EQ(ratios.zram2ufsRatio_, 100);
+    EXPECT_EQ(ratios.refaultThreshold_, 66666);
+    mem2zramRatio = 58;
+    zram2ufsRatio = 85;
+    refaultThreshold = 77777;
+    ratios.SetRatiosByValue(mem2zramRatio, zram2ufsRatio, refaultThreshold);
+    EXPECT_EQ(ratios.mem2zramRatio_, 58);
+    EXPECT_EQ(ratios.zram2ufsRatio_, 85);
+    EXPECT_EQ(ratios.refaultThreshold_, 77777);
+}
+
+HWTEST_F(MemcgTest, SetRatiosTest, TestSize.Level1)
+{
+    unsigned int mem2zramRatio = 12345;
+    unsigned int zram2ufsRatio = 54321;
+    unsigned int refaultThreshold = 66666;
+    ReclaimRatios ratios(mem2zramRatio, zram2ufsRatio, refaultThreshold);
+    ratios.SetRatios(ratios);
+    EXPECT_EQ(ratios.mem2zramRatio_, 100);
+    EXPECT_EQ(ratios.zram2ufsRatio_, 100);
+    EXPECT_EQ(ratios.refaultThreshold_, 66666);
+    mem2zramRatio = 58;
+    zram2ufsRatio = 85;
+    refaultThreshold = 77777;
+    ReclaimRatios ratios2(mem2zramRatio, zram2ufsRatio, refaultThreshold);
+    ratios.SetRatios(ratios2);
+    EXPECT_EQ(ratios.mem2zramRatio_, 58);
+    EXPECT_EQ(ratios.zram2ufsRatio_, 85);
+    EXPECT_EQ(ratios.refaultThreshold_, 77777);
+}
+
+HWTEST_F(MemcgTest, ToStringTest, TestSize.Level1)
+{
+    unsigned int mem2zramRatio = 12345;
+    unsigned int zram2ufsRatio = 54321;
+    unsigned int refaultThreshold = 66666;
+    ReclaimRatios ratios(mem2zramRatio, zram2ufsRatio, refaultThreshold);
+    std::string ret = ratios.ToString();
+    EXPECT_EQ(ret, "mem2zramRatio:100 zram2ufsRatio:100 refaultThreshold:66666");
+}
+
+HWTEST_F(MemcgTest, CreateMemcgDirTest, TestSize.Level1)
+{
+    unsigned int userId = 23456;
+    UserMemcg usermemcg= UserMemcg(userId);
+    EXPECT_EQ(usermemcg.CreateMemcgDir(), true);
+    EXPECT_EQ(usermemcg.RemoveMemcgDir(), true);
+}
+
+HWTEST_F(MemcgTest, RemoveMemcgDirTest, TestSize.Level1)
+{
+    unsigned int userId = 23456;
+    UserMemcg usermemcg= UserMemcg(userId);
+    EXPECT_EQ(usermemcg.CreateMemcgDir(), true);
+    EXPECT_EQ(usermemcg.RemoveMemcgDir(), true);
+    EXPECT_EQ(usermemcg.RemoveMemcgDir(), false);
+}
+
+HWTEST_F(MemcgTest, AddProcTest, TestSize.Level1)
+{
+    unsigned int userId = 10;
+    UserMemcg usermemcg= UserMemcg(userId);
+    EXPECT_EQ(usermemcg.CreateMemcgDir(), true);
+    EXPECT_EQ(usermemcg.AddProc(userId), false);
+    EXPECT_EQ(usermemcg.RemoveMemcgDir(), true);
+}
 }
 }
